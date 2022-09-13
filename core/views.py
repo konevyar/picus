e53fb447b1a14620671f839b0959d659d0108e35
unknown <raycast31@outlook.com>
2022-09-13 23:24:45 +0300

    feat: Add post uploading

--- core/views.py
+++ core/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from django.http import HttpResponse
+from .models import Profile, Post
 
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile= Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -100,3 +103,20 @@ def settings(request):
         return redirect('settings')
 
     return render(request, 'settings.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
+
+    return HttpResponse('<h1>Upload View</h1>')
