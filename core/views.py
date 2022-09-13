7b750050af5870bcc15c24dba1ae2987c3a74925
unknown <raycast31@outlook.com>
2022-09-13 14:55:19 +0300

    feat: Add user serttings

--- core/views.py
+++ core/views.py
@@ -29,6 +29,8 @@ def signup(request):
                 user.save()
 
                 # Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=user)
@@ -68,3 +70,33 @@ def logout(request):
     return redirect('signin')
 
 
+@login_required()
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':  # User settings filling
+        # User doesn't have profile image
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        # User have profile image
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
